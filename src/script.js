8e5e762dc5ec7b57f673da6ba1ca8598a0c634db
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-10-31 09:34:17 +0700

    create object

--- src/script.js
+++ src/script.js
@@ -2,4 +2,59 @@ import {
   Scene,
   PerspectiveCamera,
   WebGLRenderer,
+  ACESFilmicToneMapping,
+  sRGBEncoding,
+  PCFSoftShadowMap,
+  DirectionalLight,
+  Color,
+  Mesh,
+  SphereGeometry,
+  MeshPhysicalMaterial,
 } from "https://cdn.skypack.dev/three";
+
+const scene = new Scene();
+
+const camera = new PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 1000);
+
+camera.position.set(0, 15, 50);
+
+const renderer = new WebGLRenderer({ antialias: true, alpha: true });
+
+renderer.setSize(innerWidth, innerHeight);
+renderer.toneMapping = ACESFilmicToneMapping;
+renderer.outputEncoding = sRGBEncoding;
+renderer.physicallyCorrectLights = true;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = PCFSoftShadowMap;
+
+document.body.appendChild(renderer.domElement);
+
+const sunLight = new DirectionalLight(
+  new Color("#FFFFFF").convertSRGBToLinear(),
+  3.5
+);
+sunLight.position.set(10, 20, 10);
+sunLight.castShadow = true;
+sunLight.shadow.mapSize.width = 512;
+sunLight.shadow.mapSize.height = 512;
+sunLight.shadow.camera.near = 0.5;
+sunLight.shadow.camera.far = 100;
+sunLight.shadow.camera.left = -10;
+sunLight.shadow.camera.right = 10;
+sunLight.shadow.camera.top = 10;
+sunLight.shadow.camera.bottom = -10;
+scene.add(sunLight);
+
+let sphere = new Mesh(
+  new SphereGeometry(10, 70, 70),
+  new MeshPhysicalMaterial({})
+);
+
+sphere.receiveShadow = true;
+scene.add(sphere);
+
+(async () => {
+  renderer.setAnimationLoop(() => {
+    renderer.render(scene, camera);
+  });
+})();
